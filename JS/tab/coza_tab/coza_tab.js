9c4a40d191e5e7c7b9b4bf2859de5d8d0bf98171
백진선 <deck02@naver.com>
2025-04-17 17:48:59 +0900

    250417

--- JS/tab/coza_tab/coza_tab.js
+++ JS/tab/coza_tab/coza_tab.js
@@ -5,6 +5,7 @@ for(let i=0;i<btns.length;i++){
   btns[i].addEventListener("click", tabIdCalculator);
 }
 
+//초기화
 // window.addEventListener("load", defaultShow);
 
 // function defaultShow(){
@@ -17,6 +18,17 @@ for(let i=0;i<btns.length;i++){
 
 activeTab(btns[0]);
 showTabContent("all");
+
+
+// let imgEnter = document.querySelector(".imgbox");
+// imgEnter.addEventListener("mouseenter", function(){
+//   imgEnter.img.style.transform = "scale(1.2)";
+// });
+
+// let imgLeave = document.querySelector(".imgbox");
+// imgLeave.addEventListener("mouseleave", function(){
+//   imgEnter.img.style.transform = "scale(1.0)";
+// });
 
 function tabIdCalculator(){
   let tabId = this.id;
@@ -33,7 +45,7 @@ function activeTab(btn){
   btn.classList.add("btnCSS");
 }
 
-// 선택된 버튼의 id와 동일한 class 값을 가진 content
+// 선택된 버튼의 id와 동일한 class 값을 가진 content show
 function showTabContent(tabId){
   for(let i=0;i<contents.length;i++){
     contents[i].classList.remove("showContent");
@@ -46,3 +58,59 @@ function showTabContent(tabId){
     }
   }
 }
+
+
+let modal = document.querySelector(".modal");
+let images = document.querySelectorAll("img");
+
+for(let i=0;i<images.length;i++){
+  images[i].addEventListener("click", showModal);
+}
+
+function showModal(){
+  let imgID = this.id + "_modal";
+  console.log(imgID);
+
+  let modal_img = document.querySelector("." + imgID);
+  if(modal_img){
+    modal.style.display = "block";
+    modal_img.style.display = "block";
+  }
+}
+
+
+let close = document.querySelectorAll(".close");
+for(let i=0;i<close.length;i++){
+  close[i].addEventListener("click", closeModal);
+}
+
+
+modal.addEventListener("click", function(event){
+  // this: , target: 실제로 이벤트가 발생한 구체적인 영역
+  if(event.target === modal){
+    for(let i=0;i<images.length;i++){
+      modal.style.display = "none";
+    }
+  }
+});
+
+function closeModal(){
+  for(let i=0;i<images.length;i++){
+    modal.style.display = "none";
+  }
+}
+
+
+// header 고정
+let headerTop = document.querySelector(".h_top");
+let offset = headerTop.offsetHeight;
+
+window.addEventListener("scroll", function(){
+  if(this.window.scrollY > offset){
+    headerTop.classList.add("scrolled");
+    // headerBottom.style.top = 0;
+  } else{
+    headerTop.classList.remove("scrolled");
+    // headerBottom.style.top = offset + "px";
+  }
+})
