fe5485b53017984735fcea261347e60a98bc517e
백진선 <deck02@naver.com>
2025-04-21 17:48:14 +0900

    250421

--- example/fruitables/javascript.js
+++ example/fruitables/javascript.js
@@ -3,13 +3,13 @@ let menuTab = document.querySelector(".menuTab")
 let submenu = document.querySelector(".submenu");
 menuTab.addEventListener("click", submenuShow);
 
-let cnt=0;
-function submenuShow(){
+let cnt = 0;
+function submenuShow() {
   cnt++;
-  if(cnt%2 == 1){
+  if (cnt % 2 == 1) {
     // submenu.classList.add("show");
     submenu.style.display = "block";
-  } else{
+  } else {
     // submenu.classList.remove("show");
     submenu.style.display = "none";
   }
@@ -26,11 +26,9 @@ home.style.color = "#81c408";
 // slide
 let left = document.querySelector(".btn_left");
 let right = document.querySelector(".btn_right");
-let slides = document.querySelectorAll(".sliders li");
-console.log(slides);
+let slides = document.querySelectorAll(".slide");
+let slideUl = document.querySelector(".sliders")
 
-left.addEventListener("click", leftsliding);
-right.addEventListener("click", rightsliding);
 
 // let leftClicked = false;
 // let rightClicked = false;
@@ -74,6 +72,10 @@ right.addEventListener("click", rightsliding);
 //   }
 // });
 
+left.addEventListener("click", leftsliding);
+right.addEventListener("click", rightsliding);
+
+
 let active = null; // 'left' or 'right'
 
 left.addEventListener("click", () => {
@@ -114,40 +116,82 @@ right.addEventListener("mouseout", () => {
 });
 
 
+let firstContent = slides[0].cloneNode(true);
+let lastContent = slides[slides.length - 1].cloneNode(true);
+slideUl.appendChild(firstContent);  //firstContent를 slideUl의 맨 마지막 자식으로 삽입
+slideUl.insertBefore(lastContent, slides[0]); //lastContent를 slideUl의 자식 slides[0] 앞에 삽입
+slides = document.querySelectorAll(".slide");
+// 최종 slide -> slide[1] slide[0] slide[1] slide[0]
+
 setInterval(rightsliding, 3000);
 
-let index=0;
-function leftsliding(){
+// 초기 디자인
+let index = 1;
+sliding(false); //애니메이션 없이 바로 위치 이동
+
+
+function leftsliding() {
   index--;
-  if(index < 0){
-    index = slides.length -1;
+  // 복제된 맨 앞 슬라이드로 이동한 경우
+  if (index <= 0) {
+    sliding(true);  //애니메이션 적용
+    index = slides.length - 2;  //진짜 마지막으로 이동
+
+    // 0.3초 후 순간이동
+    setTimeout(() => {
+      sliding(false);
+    }, 300);
+  } else {
+    sliding(true);
   }
-  
-  slides.forEach(function(slide){
-    slide.style.transform = `translate(${index * (-100)}%)`;
-  })
 }
 
-function rightsliding(){
+function rightsliding() {
   index++;
-  if(index >= slides.length){
-    index = 0;
+  // 복제된 맨 끝 슬라이드로 이동한 경우
+  if(index >= slides.length -1){
+    sliding(true);  //애니메이션 적용
+    index = 1;  //진짜 첫 번째로 이동
+
+    // 0.3초 후 순간이동
+    setTimeout(() => {
+      sliding(false);
+    }, 300);
+  } else{
+    sliding(true);
   }
-  
-  slides.forEach(function(slide){
-    slide.style.transform = `translate(${index * (-100)}%)`;
-  })
 }
 
-function changeCSS_left(){
+// state == true: 전환 효과 on, state == false: 전환 효과 off
+function sliding(state){
+  // 슬라이드 전환 효과 on/off
+  if(state == true){
+    slides.forEach(function(slide){
+      slide.style.transition = 'all .3s';
+    })
+  } else{
+    slides.forEach(function(slide){
+      slide.style.transition = "none";
+    })
+  }
+
+  // 슬라이드 위치 이동
+  slides.forEach(function(slide){
+    slide.style.transform = `translateX(${index * (-100)}%)`;
+  })
+
+  // return "transition: " + slides[0].style.transition;
+}
+
+function changeCSS_left() {
   left.style.opacity = 1;
 }
 
-function changeCSS_right(){
+function changeCSS_right() {
   right.style.opacity = 1;
 }
 
-function changeCSS_out(){
+function changeCSS_out() {
   right.style.opacity = 0.5;
   left.style.opacity = 0.5;
 }
@@ -158,14 +202,14 @@ let tabs = document.querySelectorAll(".tabbtn");
 let contents = document.querySelectorAll(".all");
 console.log(tabs);
 
-for(let i=0;i<tabs.length;i++){
+for (let i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener("click", tabShow);
 }
 
 displayContent("all");
 changebg("all");
 
-function tabShow(){
+function tabShow() {
   let crrntID = this.id;
   console.log(crrntID);
 
@@ -173,29 +217,29 @@ function tabShow(){
   changebg(crrntID);
 }
 
-function displayContent(crrntID){
+function displayContent(crrntID) {
   let show = document.querySelectorAll("." + crrntID);
   console.log(show);
 
-  for(let i=0;i<contents.length;i++){
+  for (let i = 0; i < contents.length; i++) {
     contents[i].style.display = "none";
   }
 
-  for(let i=0;i<show.length;i++){
+  for (let i = 0; i < show.length; i++) {
     show[i].style.display = "block";
   }
 }
 
-function changebg(crrntID){
+function changebg(crrntID) {
   let bg = document.querySelector("#" + crrntID);
-  for(let i=0;i<tabs.length;i++){
+  for (let i = 0; i < tabs.length; i++) {
     tabs[i].style.backgroundColor = "#F4F6F8";
   }
   bg.style.backgroundColor = "#ffb524";
 }
 
 // advertise 색상
-window.onload = function(){
+window.onload = function () {
   let borderColor = ["#ffb524", "#45595b", "#81c408"];
   let txtboxColor = ["#81c408", "white", "#ffb524"];
   let subtitleColor = ["white", "#81c408", "white"];
@@ -205,11 +249,11 @@ window.onload = function(){
   let txtboxes = document.querySelectorAll("#advertise .txtbox");
   let subtitles = document.querySelectorAll("#advertise .txtbox p");
 
-  for(let i=0;i<3;i++){
+  for (let i = 0; i < 3; i++) {
     borders[i].style.border = `1px solid ${borderColor[i]}`;
     boxes[i].style.backgroundColor = borderColor[i];
     txtboxes[i].style.backgroundColor = txtboxColor[i];
-    subtitles[i].style.color = subtitleColor[i]; 
+    subtitles[i].style.color = subtitleColor[i];
   }
 }
 
@@ -218,10 +262,10 @@ let hTop = document.querySelector("#h_top");
 let hBottom = document.querySelector("#h_bottom");
 let offset = hTop.offsetHeight;
 
-window.addEventListener("scroll", function(){
-  if(window.scrollY > offset){
+window.addEventListener("scroll", function () {
+  if (window.scrollY > offset) {
     hBottom.classList.add("scrolled");
-  } else{
+  } else {
     hBottom.classList.remove("scrolled");
   }
 })
@@ -236,11 +280,11 @@ let modal = document.querySelector("#modal");
 let searchbtn = document.querySelector(".search");
 console.log(searchbtn);
 
-searchbtn.addEventListener("click", function(){
+searchbtn.addEventListener("click", function () {
   modal.style.display = "block";
 });
 
 let close = document.querySelector(".close");
-close.addEventListener("click", function(){
+close.addEventListener("click", function () {
   modal.style.display = "none";
 });
---
+++ example/ohou/javascript.js
@@ -0,0 +1,183 @@
+// 글쓰기 dropdown
+let btnWrite = document.querySelector(".wrtbtn");
+let dropdownWrite = document.querySelector(".wrtdropdown");
+
+btnWrite.addEventListener("click", function(){
+  dropdownWrite.classList.toggle("show");
+});
+
+// fixed
+let hTop = document.querySelector("#h_top");
+let offset = hTop.offsetHeight;
+
+window.addEventListener("scroll", function(){
+  if(window.scrollY > offset){
+    hTop.classList.add("scrolled");
+  } else{
+    hTop.classList.remove("scrolled");
+  }
+});
+
+// rank slide
+let slidesRank = document.querySelectorAll('.rnkslide');
+let slideUlRank = document.querySelector('.rank');
+
+let firstContent = slidesRank[0].cloneNode(true);
+let lastContent = slidesRank[slidesRank.length-1].cloneNode(true);
+slideUlRank.appendChild(firstContent);
+slideUlRank.insertBefore(lastContent, slidesRank[0]);
+slidesRank = document.querySelectorAll('.rnkslide');
+
+setInterval(slide, 2000);
+
+let indexRank = 1;
+slidingRank(false);
+
+function slide(){
+  indexRank++;
+  if(indexRank >= slidesRank.length-1){
+    slidingRank(true);
+    indexRank = 1;
+    setTimeout(function(){
+      slidingRank(false);
+    }, 300);
+  } else{
+    slidingRank(true);
+  }
+}
+
+function slidingRank(state){
+  if(state == true){
+    slidesRank.forEach(function(slide){
+      slide.style.transition = 'all .3s';
+    })
+  } else{
+    slidesRank.forEach(function(slide){
+      slide.style.transition = 'none';
+    })  
+  }
+
+  slidesRank.forEach(function(slide){
+    slide.style.transform = `translateY(${indexRank * (-100)}%)`;
+  })
+}
+
+// rank dropdown
+let btnRank = document.querySelector(".rnkbtn");
+let dropdownRank = document.querySelector(".rnkdropdown");
+
+btnRank.addEventListener("click", function(){
+  dropdownRank.classList.add("show");
+});
+
+let btnRank_close = document.querySelector(".rnkbtnClose");
+btnRank_close.addEventListener("click", function(){
+  dropdownRank.classList.remove("show");
+});
+
+
+
+// banner slide
+let slidesBanner = document.querySelectorAll(".slide");
+let slideUlBanner = document.querySelector(".slides");
+
+let firstContent2 = slidesBanner[0].cloneNode(true);
+let lastContent2 = slidesBanner[slidesBanner.length-1].cloneNode(true);
+slideUlBanner.appendChild(firstContent2);
+slideUlBanner.insertBefore(lastContent2, slidesBanner[0]);
+slidesBanner = document.querySelectorAll('.slide');
+
+let left = document.querySelector('.left');
+let right = document.querySelector('.right');
+
+left.addEventListener("click", slideLeft);
+right.addEventListener("click", slideRight);
+
+setInterval(slideRight, 3000);
+
+let indexBanner = 1;
+slidingBanner(false);
+
+function slideLeft(){
+  indexBanner--;
+  if(indexBanner <= 0){
+    slidingBanner(true);
+    indexBanner = slidesBanner.length -2;
+
+    setTimeout(() => {
+      slidingBanner(false);
+    }, 300);
+  } else{
+    slidingBanner(true);
+  }
+}
+
+function slideRight(){
+  indexBanner++;
+  if(indexBanner >= slidesBanner.length-1){
+    slidingBanner(true);
+    indexBanner = 1;
+
+    setTimeout(() => {
+      slidingBanner(false);
+    }, 300);
+  } else{
+    slidingBanner(true);
+  }
+}
+
+function slidingBanner(state){
+  if(state == true){
+    slidesBanner.forEach(function(slide){
+      slide.style.transition = 'all .3s';
+    })
+  } else{
+    slidesBanner.forEach(function(slide){
+      slide.style.transition = 'none';
+    })  
+  }
+
+  slidesBanner.forEach(function(slide){
+    slide.style.transform = `translateX(${indexBanner * (-100)}%)`;
+  })
+}
+
+// menuTab dropdown
+let btnMenuTab = document.querySelector(".mntbbtn");
+let dropdownMenuTab = document.querySelector(".mntbdropdown");
+
+btnMenuTab.addEventListener("click", function(){
+  dropdownMenuTab.classList.toggle("show");
+});
+
+// css
+let leftTopMenu = document.querySelectorAll(".menu li a");
+let menuTab = document.querySelectorAll(".menuTab a");
+
+  //초기 css
+  leftTopMenu[0].classList.add("changeCSS");
+  menuTab[0].classList.add("changeCSS");
+
+for(let i=0;i<leftTopMenu.length;i++){
+  leftTopMenu[i].addEventListener("click", changeTopMenu);
+}
+
+for(let i=0;i<menuTab.length;i++){
+  menuTab[i].addEventListener("click", changeBottomMenu);
+}
+
+function changeTopMenu(){
+  for(let i=0;i<leftTopMenu.length;i++){
+    leftTopMenu[i].classList.remove("changeCSS");
+  }
+  console.log(this);
+  this.classList.add("changeCSS");
+}
+
+function changeBottomMenu(){
+  for(let i=0;i<menuTab.length;i++){
+    menuTab[i].classList.remove("changeCSS");
+  }
+  console.log(this);
+  this.classList.add("changeCSS");
+}
