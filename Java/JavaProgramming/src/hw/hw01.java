8a9fb4be74cab030e4db8e44443997a39bc7ad22
백진선 <deck02@naver.com>
2025-05-02 17:47:35 +0900

    250502

--- Java/JavaProgramming/src/hw/hw01.java
+++ Java/JavaProgramming/src/hw/hw01.java
@@ -1,12 +1,28 @@
 package hw;
 import java.util.*;
 
-// 
+// 변수, 연산자, 제어문, 배열, 클래스
+// 숫자 맞추기, 가위바위보, 야구게임-숫자로 해야하는데 문자로 하고 있ㅇ네 ㅋ
 public class hw01 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner();
+		Scanner sc = new Scanner(System.in);
 		
+		String result = sc.nextLine();
+		
+		int len = result.length();
+		char[] answer = result.toCharArray();
+		
+//		for (char s : answer) {
+//		    System.out.println(s);
+//		}
+		
+		String userInput="";
+		while(userInput.equals(result)) {
+			userInput = sc.nextLine();
+			char[] userAnswer = userInput.toCharArray();
+			for()
+		}
 		
 	}
 
