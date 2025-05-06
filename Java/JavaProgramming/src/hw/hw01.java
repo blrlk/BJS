c7f48923ae680dc7a437742dc806673c5498dc4c
백진선 <deck02@naver.com>
2025-05-06 21:48:10 +0900

    hw01

--- Java/JavaProgramming/src/hw/hw01.java
+++ Java/JavaProgramming/src/hw/hw01.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class hw01 {
 
 	public static void main(String[] args) {
-		System.out.println("== 숫자 야구 시작 ==");
+		System.out.println("== 숫자 야구 시작 ==\n중복 제외");
 		Scanner sc = new Scanner(System.in);
 
 		int[] answer = new int[3]; // 정답
@@ -48,11 +48,11 @@ public class hw01 {
 			System.out.print("세 자리 숫자 입력 ");
 			int num = sc.nextInt();
 
-			// 각 자리수를 배열 안에 넣기
-			String str = String.valueOf(num);	//num 문자열로 변경
+			// 각각의 숫자 배열 안에 넣기
+			String str = String.valueOf(num);	//num을 문자열로 변경 -> 각 자리수 배열에 넣기 위함
 
 			for (int i = 0; i < answer.length; i++) {
-				userInput[i] = str.charAt(i) - '0';
+				userInput[i] = str.charAt(i) - '0';	//문자열의 i번째 문자에서 문자 '0'을 빼면 i번째 문자가 정수가 됨
 			}
 
 			// game start
