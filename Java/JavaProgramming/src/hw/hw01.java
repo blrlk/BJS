bf27124899b23da2c3a6545b972ee75dce9a39ab
백진선 <deck02@naver.com>
2025-05-07 09:48:24 +0900

    hw01

--- Java/JavaProgramming/src/hw/hw01.java
+++ Java/JavaProgramming/src/hw/hw01.java
@@ -2,10 +2,11 @@ package hw;
 
 import java.util.*;
 
+// 변수, 연산자, 제어문, 배열, 클래스
 public class hw01 {
 
 	public static void main(String[] args) {
-		System.out.println("== 숫자 야구 시작 ==\n중복 제외");
+		System.out.println("== 숫자 야구 시작 ==\n<   중복 제외   >");
 		Scanner sc = new Scanner(System.in);
 
 		int[] answer = new int[3]; // 정답
@@ -46,13 +47,32 @@ public class hw01 {
 			cnt++;
 
 			System.out.print("세 자리 숫자 입력 ");
-			int num = sc.nextInt();
+			int num = sc.nextInt();			
 
 			// 각각의 숫자 배열 안에 넣기
 			String str = String.valueOf(num);	//num을 문자열로 변경 -> 각 자리수 배열에 넣기 위함
 
 			for (int i = 0; i < answer.length; i++) {
 				userInput[i] = str.charAt(i) - '0';	//문자열의 i번째 문자에서 문자 '0'을 빼면 i번째 문자가 정수가 됨
+			}
+			
+			// 사용자 입력 숫자 중복 여부 검사
+			boolean isused = false;
+			for(int i=0;i<userInput.length;i++) {
+				
+				for(int j=0;j<userInput.length;j++) {
+					if(userInput[i] == userInput[j]) {
+						if(i!=j) {
+							isused = true;
+							break;
+						}
+					}
+				}
+			}
+			
+			if(isused) {
+				System.out.println("중복된 숫자는 입력 불가능");
+				continue;
 			}
 
 			// game start
