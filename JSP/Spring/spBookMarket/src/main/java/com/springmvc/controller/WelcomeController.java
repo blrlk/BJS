ed1ad1b5e181514fb8bff58c9b02de258a9dc68c
백진선 <deck02@naver.com>
2025-06-16 16:26:47 +0900

    250616

---
+++ JSP/Spring/spBookMarket/src/main/java/com/springmvc/controller/WelcomeController.java
@@ -0,0 +1,24 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {
+	
+	WelcomeController(){
+		System.out.println("WelcomeController 객체 생성 완료");
+	}
+
+	@RequestMapping(value="/home", method=RequestMethod.GET)
+	public String welcome(Model model) {
+		System.out.println("WelcomController의 welcome 함수 입장 완료");
+		model.addAttribute("greeting", "Welcome to BookMarket");
+		model.addAttribute("strapline", "Welcome to WebShopping Mall!");
+		
+		return "welcome";
+	}
+
+}
